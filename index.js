0387da110137ed2f460faf4e514febf6fc70f8f3
ivanpo1 <ivan.poppino@gmail.com>
2025-06-26 16:01:27 -0300

    added delete and update functionality

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const express = require('express')
 const Person = require('./models/person')
 
 const app = express()
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
 
 
 const morgan = require('morgan')
@@ -41,11 +41,12 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', async(request, response) => {
@@ -58,7 +59,7 @@ app.post('/api/persons', async(request, response) => {
     }
 
 
-    const alreadyExist = await Person.findOne({name: body.name})
+    const alreadyExist = await Person.findOne({ name: body.name })
     if (alreadyExist) {
         return response.status(400).json({
             error: 'name must be unique'
@@ -75,6 +76,43 @@ app.post('/api/persons', async(request, response) => {
     })
 
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const { number } = request.body
+    console.log('number in request.body', number)
+
+    Person.findById(request.params.id)
+        .then(person => {
+            if(!person) {
+                return response.status(404).end()
+            }
+
+            person.number = number
+
+            return person.save().then((updatedPerson) => {
+                response.json(updatedPerson)
+            })
+        })
+        .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
